f8653023eb2a032752489b55511c1d1cb36a2569
_N3m0 <n3m0o7@hotmail.com>
2024-02-05 11:39:23 +0100

    refacto get user input

--- main.c
+++ main.c
@@ -5,60 +5,75 @@
 #define MAX_FILE_NB 64
 #define INIT_SIZE_BUFFER 1024
 
-void getFileName(const int argc, char ** argv, char **buffer, int *file_len, int *skip_buffer, int *skip_len, const int max){
-  *skip_len = 0;
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+void getUserInput(int argc, char **argv, char **file, int *file_len,  char *program,  int *program_len, int *program_cap){
+  int skip_buffer[MAX_FILE_NB];
+  int skip_len = 0;
   *file_len = 0;
   for (int i=1; i<argc-1; i++){
-    if (*file_len >= max){
+    if (*file_len >= MAX_FILE_NB){
       printf("ERROR: cannot parse more than %d file.\n", MAX_FILE_NB);
       exit(-1);
     }
 
     if (strncmp(argv[i], "--file", 6) == 0){
-      skip_buffer[(*skip_len)++] = i;
+      skip_buffer[skip_len++] = i;
       if (*(argv[i]+6) != '\0')
-        buffer[(*file_len)++] = argv[i]+2;
+        file[(*file_len)++] = argv[i]+2;
       else{
-        buffer[(*file_len)++] = argv[++i];
-        skip_buffer[(*skip_len)++] = i;
+        file[(*file_len)++] = argv[++i];
+        skip_buffer[skip_len++] = i;
       }
     }
   }
-}
 
-void cleanArgv(const int argc, char **argv, char *buffer, int *buffer_len, const int *skip, const int skip_len, const int init_len){
   int skip_cursor = 0;
-  *buffer_len = 0;
+  *program_len = 0;
   for (int i=1; i<argc; i++){
-    if (skip_cursor < skip_len && skip[skip_cursor] == i){
+    if (skip_cursor < skip_len && skip_buffer[skip_cursor] == i){
       skip_cursor++;
       continue;
     }
 
     for (int j=0; j<1024 && *(argv[i]+j) != '\0'; j++){
-      if (*buffer_len >= init_len) break;
-      buffer[(*buffer_len)++] = *(argv[i]+j);
+      if (*program_len >= *program_cap){
+        *program_cap *= 2;
+        program = realloc(program, *program_cap);
+      }
+      program[(*program_len)++] = *(argv[i]+j);
     }
+    if (*program_len >= *program_cap){
+      *program_cap *= 2;
+      program = realloc(program, *program_cap);
+    }
+    program[(*program_len)++] = ' ';
   }
 }
 
+void evaluate(char *program, int len){
+  (void) program;
+  (void) len;
+}
+
+void evaluate_from_file(char *file){
+  (void) file;
+}
+
 int main(int argc, char **argv){
-  int skip[MAX_FILE_NB] = {0};
-  int skip_len = 0;
   char *file[MAX_FILE_NB] = {0};
   int file_len = 0;
-  getFileName(argc, argv, file, &file_len, skip, &skip_len, MAX_FILE_NB);
+  char *program = malloc(INIT_SIZE_BUFFER);
+  int program_len = 0;
+  int program_cap = INIT_SIZE_BUFFER;
 
-  char buffer[INIT_SIZE_BUFFER] = {0};
-  int buffer_len = 0;
-  cleanArgv(argc, argv, buffer, &buffer_len, skip, skip_len, INIT_SIZE_BUFFER-1);
+  getUserInput(argc, argv, file, &file_len, program, &program_len, &program_cap);
 
-  for (int i=0; i<file_len; i++) printf("file %d: %s\n", i, file[i]);
-  printf("---\n");
-  for (int i=0; i<skip_len; i++) printf("skip %d: %s\n", skip[i], argv[skip[i]]);
-  printf("---\n");
-  printf("%s\n", buffer);
-  printf("len: %d\n", buffer_len);
-  
+  for (int i=0; i<file_len; i++){
+    evaluate_from_file(file[i]);
+  }
+
+  evaluate(program, program_len);
+
 	return 0;
 }
